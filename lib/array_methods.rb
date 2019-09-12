e57420cd5619baed145590d9006a18bcb3c124b1
Daniel Lamb <daniellamb113@gmail.com>
2019-09-12 06:16:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,9 @@
 def using_include(array, element)
-array.include?(element)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    element1 = "wow"
+    element2 = "Tardis"
+    expect(using_include(array, element1)).to eq(true)
+    expect(using_include(array, element2)).to eq(false)
 end
 
 def using_sort(array)
